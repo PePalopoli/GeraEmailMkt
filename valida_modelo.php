897a8d2d0400d058b9216910b42e80d4790c9607
Palopoli <palopoli.pedro@gmail.com>
2022-06-28 13:37:04 -0300

    Validando Modelo e enviando Post

--- valida_modelo.php
+++ valida_modelo.php
@@ -2,7 +2,28 @@
 
 if($_POST)
 {
-    die(var_dump($_POST));
+    //die(var_dump($_POST));
+    switch ($_POST["iModelo"]) {
+        case '1':
+            $tipo_modelo = "modelo_1_padrao.php";
+            break;
+        
+        default:
+            $tipo_modelo = "modelo_1_padrao.php";
+            break;
+    }
+
+    $url = 'https://alcateiawebdigital.com.br/systememail/assets/modelos_email/'. $tipo_modelo;
+    $myvars = $_POST;
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $myvars);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    $response = curl_exec($ch);
 }
 
 
